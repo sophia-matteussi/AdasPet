9ab79fa4415e3ea76c1c192e1450c611da4b75c8
Jeniffer04 <jcristinasteilein@gmail.com>
2021-10-07 17:28:42 -0300

    CadastroProdutosNF

---
+++ Areas/Identity/Pages/CadastroProdutos.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdasPet.Areas.Identity.Pages
+{
+    public class CadastroProdutosModel : PageModel
+    {      
+        public IActionResult OnGet()
+        {
+            return Page();
+
+        }
+    }
+}
---
+++ Controllers/PagesController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdasPet.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: PagesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PagesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PagesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PagesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PagesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PagesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PagesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PagesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public IActionResult CadastroProdutos()
+        {
+            return View();
+        }
+    }
+}
